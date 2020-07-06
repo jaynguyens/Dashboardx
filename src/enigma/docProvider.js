f1f81b8e12bcd5f02a45b275ae30f57c715f1f0d
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-06 16:47:25 +0100

    no more console.log

--- src/enigma/docProvider.js
+++ src/enigma/docProvider.js
@@ -1,25 +1,26 @@
-import React, { useState, useEffect, createContext } from "react";
-import { openSession, closeSession } from "./configSession";
+import React, { useState, useEffect, createContext } from "react"
+import { openSession, closeSession } from "./configSession"
 
-export const QDocContext = createContext();
+export const QDocContext = createContext()
 
 const DocProvider = ({ children }) => {
-	const [qDoc, setqDoc] = useState();
+	const [qDoc, setqDoc] = useState()
 
 	useEffect(() => {
 		const openDoc = async () => {
-			setqDoc(await openSession());
-		};
-		openDoc();
-		return closeSession;
-	}, []);
+			setqDoc(await openSession())
+		}
+		openDoc()
+		return closeSession
+	}, [])
 	return (
 		<React.StrictMode>
 			{qDoc && (
-				<QDocContext.Provider value={qDoc}>{children}</QDocContext.Provider>
+				<QDocContext.Provider value={qDoc}>
+					{children}
+				</QDocContext.Provider>
 			)}
 		</React.StrictMode>
-	);
-};
-console.log("QDContext", QDocContext);
-export default DocProvider;
+	)
+}
+export default DocProvider
