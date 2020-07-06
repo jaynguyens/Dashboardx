d7798aff4e8d075db8ae6111d85e069c2dc4cd09
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-06 11:26:42 +0100

    Make a wrapper around the main app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import DocProvider from "./enigma/docProvider";
 ReactDOM.render(
    <React.StrictMode>
-      <App />
+      <DocProvider>
+         <App />
+      </DocProvider>
    </React.StrictMode>,
    document.getElementById("root")
 );
