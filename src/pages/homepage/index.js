3297c313560e7156f0285b86ee7d951556521d59
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-06 17:12:10 +0100

    some styling for the homepage

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,12 +1,39 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import styled from "styled-components"
+
 const Homepage = () => {
 	return (
-		<div>
-			Homepage
-			<Link to="/dashboard">Access Dashboard</Link>
-		</div>
+		<Div>
+			<h1>Insurance Claims 2020</h1>
+			<Button to="/dashboard">Access Dashboard</Button>
+		</Div>
 	)
 }
 
 export default Homepage
+
+const Div = styled.div`
+	display: flex;
+	height: 100vh;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	background-image: url("/email-pattern.png");
+`
+const Button = styled(Link)`
+	margin: 1rem;
+	text-transform: uppercase;
+	text-decoration: none;
+	font-weight: bold;
+	color: darkorange;
+	padding: 20px;
+	border: 4px solid #494949;
+	transition: all 0.4s ease 0s;
+	&:hover {
+		color: #fff;
+		background-color: #f6b93b;
+		border-color: #f6b93b;
+		transition: all 0.4s ease 0s;
+	}
+`
