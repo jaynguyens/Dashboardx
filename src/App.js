c524ba96938fb2a0b83e3286a03c95803da42610
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-06 14:36:35 +0100

    testin definition for enigma

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useContext } from "react"
+import { QDocContext } from "./enigma/docProvider"
+import { SummaryClaimType } from "./enigma/definition/summary/claimtype"
+
 function App() {
-	return <div className="App">hello this is App.js</div>;
+	const qdoc = useContext(QDocContext)
+	useEffect(() => {
+		const ClaimType = async () => {
+			const session = await qdoc.createSessionObject(SummaryClaimType)
+			//const layout = await session.getLayout()
+			//const dataset = layout.qHyperCube.qDataPages[0].qMatrix
+
+			console.log(session)
+		}
+		ClaimType()
+	}, [qdoc])
+
+	return <div className="App">hello this is App.js</div>
 }
 
-export default App;
+export default App
---
+++ src/enigma/definition/summary/claimtype.js
@@ -0,0 +1,33 @@
+export const SummaryClaimType = {
+	qInfo: {
+		qType: "stackbarchart",
+	},
+	qHyperCubeDef: {
+		qDimensions: [
+			{
+				qDef: {
+					qFieldDefs: ["Claim Notification Date.Year"],
+				},
+			},
+		],
+		qMeasures: [
+			{
+				qDef: {
+					qDef:
+						"Sum($<[Notification Date.autoCalendar.InYTD]={1}>}[ClaimCounter])",
+				},
+			},
+		],
+		qMode: "EQ_DATA_MODE_PIVOT_STACK",
+		qNoOfLeftDims: -1,
+		qAlwaysFullyExpanded: true,
+		qInitialDataFetch: [
+			{
+				qTop: 0,
+				qLeft: 0,
+				qWidth: 5,
+				qHeight: 50,
+			},
+		],
+	},
+}
