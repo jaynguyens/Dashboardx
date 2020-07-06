42e3d07b5c41e4b97a246920caac6258441a9939
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-06 16:21:18 +0100

    for now, app only call routes component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react"
 import Routes from "./Routes"
 // App function render the routing and layout of the dashboard
-function App() {
+const App = () => {
 	return (
-		<div className="App">
+		<>
 			<Routes />
-		</div>
+		</>
 	)
 }
 
