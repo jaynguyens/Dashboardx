f49a5efce6fc55958039bb8e6df1f19fa2ed94c8
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-06 15:54:46 +0100

    typo

---
+++ src/enigma/definition/summary/claimstype.js
@@ -0,0 +1,46 @@
+const SummaryClaimsType = {
+	qInfo: {
+		qType: "stackbarchart",
+	},
+
+	// What is a hypercube?
+	// in basic term, it's a table with rows and columns
+	// https://qlik.dev/libraries-and-tools/nebulajs/hypercube-introduction
+	qHyperCubeDef: {
+		qDimensions: [
+			{
+				qDef: {
+					qFieldDefs: ["Claim Notification Date.autoCalendar.Year"],
+				},
+			},
+			{
+				qDef: {
+					qFieldDefs: ["Claim Type"],
+				},
+			},
+		],
+		qMeasures: [
+			{
+				qDef: {
+					qDef:
+						"Sum( { $< [Claim Notification Date.autoCalendar.InYTD]={1} > } [ClaimCounter] )",
+					qLabel: "Claims Opened",
+				},
+			},
+		],
+		// qMode default is "S" - straight mode
+		// qMode: "EQ_DATA_MODE_PIVOT_STACK",
+		// qNoOfLeftDims: -1,
+		qAlwaysFullyExpanded: true,
+		qInitialDataFetch: [
+			{
+				qTop: 0,
+				qLeft: 0,
+				qWidth: 5,
+				qHeight: 100,
+			},
+		],
+	},
+}
+
+export default SummaryClaimsType
