5475ddea9f6ea0885357815f169d62c3b813195d
Jay Nguyen <jnguyen@calibrateconsulting.com>
2020-07-06 15:00:31 +0100

    Delete session.close(), app stay remain open data

--- src/enigma/configSession.js
+++ src/enigma/configSession.js
@@ -2,30 +2,27 @@ import enigma from "enigma.js";
 import schema from "enigma.js/schemas/12.67.2.json";
 
 const config = {
-   host: process.env.REACT_APP_QLIK_HOST,
-   port: process.env.REACT_APP_QLIK_PORT,
-   secure: process.env.REACT_APP_QLIK_SECURE,
-   prefix: process.env.REACT_APP_QLIK_PREFIX,
-   appId: process.env.REACT_APP_QLIK_APPID
+	host: process.env.REACT_APP_QLIK_HOST,
+	port: process.env.REACT_APP_QLIK_PORT,
+	secure: process.env.REACT_APP_QLIK_SECURE,
+	prefix: process.env.REACT_APP_QLIK_PREFIX,
+	appId: process.env.REACT_APP_QLIK_APPID,
 };
 
 const url = (host, port) => {
-   const portUrl = id => (id ? `:${id}` : ``);
-   console.log(`ws://${host}${portUrl(port)}/app`);
-   return `ws://${host}${portUrl(port)}/app`;
+	const portUrl = (id) => (id ? `:${id}` : ``);
+	return `ws://${host}${portUrl(port)}/app`;
 };
 
 const session = enigma.create({
-   schema,
-   url: url(config.host, config.port),
-   suspendOnClose: true
+	schema,
+	url: url(config.host, config.port),
 });
 
 const openSession = async () => {
-   const qix = await session.open();
-   const document = await qix.openDoc(config.appId);
-   await session.close();
-   return document;
+	const qix = await session.open();
+	const document = await qix.openDoc(config.appId);
+	return document;
 };
 
 const closeSession = () => session.close();
