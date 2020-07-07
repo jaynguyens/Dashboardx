61898abaad33ca7f74d1742d88954d4cce43deb9
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-07 10:15:53 +0100

    reset css padding and margin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import DocProvider from "./enigma/docProvider";
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import "./style/index.css"
+import DocProvider from "./enigma/docProvider"
 ReactDOM.render(
 	<DocProvider>
 		<App />
 	</DocProvider>,
 	document.getElementById("root")
-);
+)
