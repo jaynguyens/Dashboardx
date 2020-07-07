5f849c4aa3aac793907a158785d6711473374eda
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-07 14:41:15 +0100

    make a stacked bar charts

--- src/pages/summary/index.js
+++ src/pages/summary/index.js
@@ -1,28 +1,88 @@
-import React, { useEffect, useContext } from "react"
+import React, { useState, useEffect, useContext } from "react"
 import { QDocContext } from "../../enigma/docProvider"
 import SummaryClaimsType from "../../enigma/definition/summary/claimstype"
+import styled from "styled-components"
+import StackedBarChart from "./stackedBarChart"
 
 const SummaryPage = () => {
+	const [Data, setData] = useState([])
+	const [Dataset, setDataset] = useState([])
+
 	const qdoc = useContext(QDocContext)
 
+	//TODO: maybe extract the useeffect as a custom hooks
+	// with 1 argument - the chart definition sheet.
+	// return an array -q Matrix-
 	useEffect(() => {
 		const ClaimType = async () => {
 			const session = await qdoc.createSessionObject(SummaryClaimsType)
 			const layout = await session.getLayout()
 			const dataset = layout.qHyperCube.qDataPages[0].qMatrix
-
-			dataset
-				? console.log(dataset)
-				: console.log(layout.qHyperCube.qMatrix)
+			setData(dataset)
 		}
 		ClaimType()
 	}, [qdoc])
 
+	//TODO: extract data from raw dataset
+	/** GOAL is to extract data in this type of structure
+	[
+		["2016", "Windscreen", "148"],
+		["2016", "Theft", "32"],
+		["2016", "Fire", "2"]
+	]
+	 */
+
+	useEffect(() => {
+		const ExtractData = async () => {
+			setDataset(
+				Data.map((datum) =>
+					datum.map((d) => {
+						return d.qText
+					})
+				)
+			)
+		}
+		ExtractData()
+	}, [Data])
+
+	/**
+	 * pass dataset to the component
+	 */
+
 	return (
-		<div>
-			<h4>Dashboard Summary</h4>
-		</div>
+		<Div>
+			<MiniCharts />
+			<MiniCharts />
+			<MiniCharts />
+			<BigChart>
+				<StackedBarChart data={Dataset} />
+			</BigChart>
+		</Div>
 	)
 }
 
 export default SummaryPage
+
+const Div = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	background-color: lightgray;
+	height: 100vh;
+`
+
+const MiniCharts = styled.div`
+	width: 250px;
+	height: 200px;
+	background-color: #fff;
+	margin: 25px;
+	border-radius: 4px;
+	box-shadow: 0 3px 3px rgba(0, 0, 0, 0.2);
+`
+const BigChart = styled.div`
+	width: 80vw;
+	height: 600px;
+	background-color: #fff;
+	border-radius: 2px;
+	box-shadow: 0 3px 3px rgba(0, 0, 0, 0.2);
+`
---
+++ src/pages/summary/stackedBarChart.js
@@ -0,0 +1,150 @@
+import React, { useEffect, useRef } from "react"
+import * as d3 from "d3"
+
+const StackedBarChart = ({ data }) => {
+	const svgRef = useRef()
+	const wrapperRef = useRef()
+
+	//TODO:
+	const keys = ["Windscreen", "Collision", "Theft", "Fire"]
+
+	const colors = {
+		Windscreen: "green",
+		Collision: "orange",
+		Theft: "purple",
+		Fire: "blue",
+	}
+
+	const dataset = [
+		{
+			name: 2016,
+			Windscreen: 168,
+			Collision: 404,
+			Theft: 44,
+			Fire: 2,
+		},
+		{
+			name: 2017,
+			Windscreen: 730,
+			Collision: 1521,
+			Theft: 189,
+			Fire: 11,
+		},
+		{
+			name: 2018,
+			Windscreen: 918,
+			Collision: 1811,
+			Theft: 228,
+			Fire: 14,
+		},
+		{
+			name: 2019,
+			Windscreen: 1076,
+			Collision: 2409,
+			Theft: 172,
+			Fire: 11,
+		},
+		{
+			name: 2020,
+			Windscreen: 1495,
+			Collision: 3110,
+			Theft: 201,
+			Fire: 9,
+		},
+	]
+
+	useEffect(() => {
+		const { width, height } = wrapperRef.current.getBoundingClientRect()
+		const margin = {
+			top: 20,
+			right: 20,
+			bottom: 30,
+			left: 40,
+		}
+		console.log(wrapperRef.current.getBoundingClientRect())
+
+		const stackGenerator = d3
+			.stack()
+			.keys(keys)
+			.order(d3.stackOrderAscending)
+		const layers = stackGenerator(dataset)
+
+		const extent = [
+			0,
+			d3.max(layers, (layer) => d3.max(layer, (sequence) => sequence[1])),
+		]
+
+		const svg = d3
+			.select(svgRef.current)
+			.attr("width", width)
+			.attr("height", height)
+
+		// Scale
+		// domain is the limit of the axis [0, maximum value of year]
+		// range is the output in pixel
+		const scale = {
+			// x is the number of claims
+			x: d3
+				.scaleBand()
+				.domain(dataset.map((d) => d.name))
+				.range([margin.left, width - margin.right])
+				.padding(0.25),
+			// y is the year
+			y: d3
+				.scaleLinear()
+				.domain(extent)
+				.range([height - margin.bottom, margin.top]),
+		}
+
+		// Axis
+		// following the margin convention for axis
+		const axis = {
+			x: (g) =>
+				g
+					.attr(
+						"transform",
+						`translate(0, ${height - margin.bottom})`
+					)
+					.call(d3.axisBottom(scale.x)),
+			y: (g) =>
+				g
+					.attr("transform", `translate(${margin.left}, 0)`)
+					.call(d3.axisLeft(scale.y))
+					.call((g) => g.selectAll("line").remove()),
+		}
+
+		svg.select(".x-axis").call(axis.x)
+		svg.select(".y-axis").call(axis.y)
+
+		// Draw
+
+		// create a stack for each year
+		// console.log(layers)
+		svg.selectAll(".layers")
+			.data(layers)
+			.join("g")
+			.attr("class", "layer")
+			.attr("fill", (layer) => colors[layer.key])
+			.selectAll("rect")
+			.data((layer) => layer)
+			.join("rect")
+			.attr("x", (sequence) => scale.x(sequence.data.name))
+			.attr("width", scale.x.bandwidth())
+			.attr("y", (sequence) => scale.y(sequence[1]))
+			.attr(
+				"height",
+				(sequence) => scale.y(sequence[0]) - scale.y(sequence[1])
+			)
+	}, [keys, dataset, colors])
+
+	return (
+		<div ref={wrapperRef} style={{ height: "100%" }}>
+			<svg ref={svgRef}>
+				<g className="x-axis"></g>
+				<g className="y-axis"></g>
+			</svg>
+		</div>
+	)
+}
+
+export default StackedBarChart
