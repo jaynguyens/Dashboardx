ce05ea79544111335a9b4c88f8041e5de547eb6a
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-08 19:55:56 +0100

    mock example use of custom hook useselection

--- src/pages/claimsCosts/index.js
+++ src/pages/claimsCosts/index.js
@@ -1,6 +1,14 @@
 import React from "react"
+import ClaimsVehicle from "../../enigma/definition/claims-costs/vehicleInsurance"
+import useSelectionFromHyperCube from "../../hooks/useSelectionFromHyperCube"
+import useLayoutSessionObject from "../../hooks/useLayoutSessionObject"
 
 const ClaimsCosts = () => {
+	// const data = useLayoutSessionObject(ClaimsVehicle)
+	// console.log(data)
+
+	const selection = useSelectionFromHyperCube(ClaimsVehicle, [3, 5, 7, 8])
+	console.log(selection)
 	return <div>Claims Cost Analysis here</div>
 }
 
