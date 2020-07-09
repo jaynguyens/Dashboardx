0def35ec1ccc367ab8c47119fc13fc316bfb8bae
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-09 15:08:27 +0100

    minor

--- src/pages/summary/index.js
+++ src/pages/summary/index.js
@@ -67,8 +67,6 @@ const MiniCharts = styled.div`
 	border-radius: 4px;
 	box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
 		0 4px 6px -2px rgba(0, 0, 0, 0.05);
-	/* box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
-		0 2px 4px -1px rgba(0, 0, 0, 0.06); */
 `
 const BigChart = styled.div`
 	width: 80vw;
