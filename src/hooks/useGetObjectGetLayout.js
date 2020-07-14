1e42681db38cc96da259dc6e92aaecda0fc4e06a
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-14 16:48:47 +0100

    todo

--- src/hooks/useGetObjectGetLayout.js
+++ src/hooks/useGetObjectGetLayout.js
@@ -10,6 +10,10 @@ const useGetObjectGetLayout = object => {
          const model = await qdoc.getObject(object);
          const layout = await model.getLayout();
          const qTitle = layout.qHyperCube.qMeasureInfo[0].qFallbackTitle;
+         console.log(layout.qHyperCube);
+
+         // TODO: get qDimensionInfo/qGroupFallbackTitles
+         //  get qMeasureInfo/qGroupFallbackTitles
          const qMatrix = layout.qHyperCube.qDataPages[0].qMatrix;
          setData([qTitle, qMatrix]);
       })();
