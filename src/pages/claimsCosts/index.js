283cadb21195ff5f7af6331a8909560b13225251
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-14 14:31:18 +0100

    simplify the code

--- src/pages/claimsCosts/index.js
+++ src/pages/claimsCosts/index.js
@@ -1,85 +1,48 @@
-import React, { useState } from "react"
-import ClaimsVehicle from "../../enigma/definition/claims-costs/vehicleInsurance"
-import useLayoutSessionObject from "../../hooks/useLayoutSessionObject"
-import useGetObjectGetLayout from "../../hooks/useGetObjectGetLayout"
-import MiniChart from "../../components/miniChart"
-import BigChart from "../../components/bigChart"
-import Indicator from "./indicator"
-import ScatterPlot from "./scatterPlot"
-import BarChart from "./barChart"
-
-import SelectionContext from "./selectionContext"
-import ProcessData2 from "../../helper/processData2"
+import React from "react";
+import ClaimsVehicle from "../../enigma/definition/claims-costs/vehicleInsurance";
+import useLayoutSessionObject from "../../hooks/useLayoutSessionObject";
+import useGetObjectGetLayout from "../../hooks/useGetObjectGetLayout";
+import ScatterPlot from "./scatterPlot";
+import BarChart from "./barChart";
+import ProcessData2 from "../../helper/processData2";
+import Indicators from "../../components/indicators";
+import Indicator from "./indicator";
 
 const ClaimsCosts = () => {
-	const [selection, setSelection] = useState([])
-	const [toggle, setToggle] = useState(true)
-	const scatterPlotDataset = useLayoutSessionObject(ClaimsVehicle)
-	const barChartDataset = useGetObjectGetLayout("eRsGevp")
+   const scatterPlotDataset = useLayoutSessionObject(ClaimsVehicle);
+   const barChartDataset = useGetObjectGetLayout("eRsGevp");
 
-	// When dataset stil waiting from qlik
-	// data is undefined
+   if (!barChartDataset || !scatterPlotDataset) {
+      return <div> no dataset -- still wating for dataset</div>;
+   }
+   console.log(barChartDataset);
+   const datasetBarChart = ProcessData2(
+      barChartDataset,
+      "Year",
+      "Total Claim Cost"
+   );
 
-	if (!barChartDataset || !scatterPlotDataset) {
-		return <div> no dataset -- still wating for dataset</div>
-	}
+   const datasetScatterPlot = ProcessData2(
+      scatterPlotDataset,
+      "Rating Group",
+      "Ave Total Claims Cost",
+      "Ave Annual Premium",
+      "Loss Ratio"
+   );
 
-	const datasetBarChart = ProcessData2(
-		barChartDataset,
-		"Year",
-		"Total Claim Cost"
-	)
-	const datasetScatterPlot = ProcessData2(
-		scatterPlotDataset,
-		"Rating Group",
-		"Ave Total Claims Cost",
-		"Ave Annual Premium",
-		"Loss Ratio"
-	)
+   return (
+      <>
+         <Indicators>
+            <Indicator title="Ave Annual Premium" />
+            <Indicator title="Ave Total Claims Cost" />
+            <Indicator title="Loss Ratio" />
+         </Indicators>
+         <Indicators>
+            <ScatterPlot dataset={datasetScatterPlot} />
+            <BarChart dataset={datasetBarChart} />
+         </Indicators>
+      </>
+   );
+};
 
-	if (toggle) {
-		setSelection(datasetScatterPlot)
-		setToggle(false)
-	}
-
-	return (
-		<SelectionContext.Provider value={{ selection, setSelection }}>
-			<div
-				style={{
-					display: "flex",
-					flexWrap: "wrap",
-					backgroundColor: "rgba(247, 250, 252)",
-					justifyContent: "center",
-				}}
-			>
-				<MiniChart>
-					<Indicator title="Ave Annual Premium" />
-				</MiniChart>
-				<MiniChart>
-					<Indicator title="Ave Total Claims Cost" />
-				</MiniChart>
-				<MiniChart>
-					<Indicator title="Loss Ratio" />
-				</MiniChart>
-			</div>
-			<div
-				style={{
-					display: "flex",
-					flexWrap: "wrap",
-					backgroundColor: "rgba(247, 250, 252)",
-					justifyContent: "center",
-					paddingBottom: "1rem",
-				}}
-			>
-				<BigChart>
-					<ScatterPlot dataset={datasetScatterPlot} />
-				</BigChart>
-				<BigChart>
-					<BarChart dataset={datasetBarChart} />
-				</BigChart>
-			</div>
-		</SelectionContext.Provider>
-	)
-}
-
-export default ClaimsCosts
+export default ClaimsCosts;
