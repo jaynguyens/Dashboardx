d0ee63bb06a4b84ebf3c5996dee9924cbef0d812
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-14 14:26:28 +0100

    minor

--- src/Routes.js
+++ src/Routes.js
@@ -1,57 +1,62 @@
-import React from "react"
+import React from "react";
 import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	useRouteMatch,
-	useParams,
-} from "react-router-dom"
-import Homepage from "./pages/homepage"
-import SummaryPage from "./pages/summary"
-import NavBar from "./components/navBar"
-import ClaimsCosts from "./pages/claimsCosts"
+   BrowserRouter as Router,
+   Switch,
+   Route,
+   useRouteMatch,
+   useParams
+} from "react-router-dom";
+import Homepage from "./pages/homepage";
+import SummaryPage from "./pages/summary";
+import NavBar from "./components/navBar";
+import ClaimsCosts from "./pages/claimsCosts";
+
 const Routes = () => {
-	return (
-		<Router>
-			<div>
-				<Switch>
-					<Route path="/dashboard">
-						<Dashboards />
-					</Route>
-					<Route path="/">
-						<Homepage />
-					</Route>
-				</Switch>
-			</div>
-		</Router>
-	)
-}
+   return (
+      <Router>
+         <div>
+            <Switch>
+               <Route path="/dashboard">
+                  <Dashboards />
+               </Route>
+               <Route path="/">
+                  <Homepage />
+               </Route>
+            </Switch>
+         </div>
+      </Router>
+   );
+};
 
 function Dashboards() {
-	let match = useRouteMatch()
+   let match = useRouteMatch();
 
-	return (
-		<div>
-			<NavBar />
-			<Switch>
-				<Route exact path={`${match.path}/claims-costs-analysis`}>
-					<ClaimsCosts />
-				</Route>
-				<Route exact path={`${match.path}/:topicId`}>
-					<NotAvail />
-				</Route>
-				<Route exact path={match.path}>
-					<SummaryPage />
-				</Route>
-			</Switch>
-		</div>
-	)
+   return (
+      <div>
+         <NavBar />
+         <div
+            style={{ minHeight: "93vh", backgroundColor: "rgba(247,250,252)" }}
+         >
+            <Switch>
+               <Route exact path={`${match.path}/claims-costs-analysis`}>
+                  <ClaimsCosts />
+               </Route>
+               <Route exact path={`${match.path}/:topicId`}>
+                  <NotAvail />
+               </Route>
+               <Route exact path={match.path}>
+                  <SummaryPage />
+               </Route>
+            </Switch>
+         </div>
+      </div>
+   );
 }
 
 const NotAvail = () => {
-	let { topicId } = useParams()
-	return (
-		<h3>Requested dashboard ID: {topicId}. Not Available At the Moment.</h3>
-	)
-}
-export default Routes
+   let { topicId } = useParams();
+   return (
+      <h3>Requested dashboard ID: {topicId}. Not Available At the Moment.</h3>
+   );
+};
+export default Routes;
