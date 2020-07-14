47e5f076da7e87f22c2e30d27620b6fc7a406496
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-14 14:32:17 +0100

    the page of the day !! break through !!

--- src/pages/claimsCosts/barChart.js
+++ src/pages/claimsCosts/barChart.js
@@ -1,13 +1,104 @@
-import React, { useRef } from "react"
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import BigChart from "../../components/bigChart";
+import useResizeObserver from "../../hooks/useResizeObserver";
+import * as d3 from "d3";
+import useModel from "../../hooks/useModel";
+import ProcessData2 from "../../helper/processData2";
 
-const BarChart = () => {
-	const svgRef = useRef()
+const BarChart = ({ dataset }) => {
+   const [data, setData] = useState(dataset);
+   const wrapperRef = useRef();
+   const svgRef = useRef();
+   const dimension = useResizeObserver(wrapperRef);
+   const model = useModel("eRsGevp");
 
-	return (
-		<div>
-			<svg ref={svgRef}></svg>
-		</div>
-	)
-}
+   const HandleClick = useCallback(
+      async d => {
+         await model.selectHyperCubeValues(
+            "/qHyperCubeDef",
+            0,
+            [d.qElemNumber],
+            false
+         );
+         const layout = await model.getLayout();
+         const dataset = await layout.qHyperCube.qDataPages[0].qMatrix;
+         const result = ProcessData2(dataset, "Cause", "Cost");
 
-export default BarChart
+         setData(result);
+         console.log(dataset, result);
+      },
+      [model]
+   );
+
+   useEffect(() => {
+      if (!dimension) return;
+
+      const margin = {
+         top: 20,
+         bottom: 20,
+         left: 60,
+         right: 40
+      };
+      const svg = d3
+         .select(svgRef.current)
+         .attr("width", dimension.width)
+         .attr("height", dimension.height);
+
+      const scale = {
+         x: d3
+            .scaleBand()
+            .domain(data.map(d => d["Year"] || d["Cause"]))
+            .range([margin.left, dimension.width - margin.right])
+            .padding(0.2),
+         y: d3
+            .scaleLinear()
+            .domain(
+               d3.extent(data.map(d => d["Total Claim Cost"] || d["Cost"]))
+            )
+            .range([dimension.height - margin.bottom, margin.top])
+            .nice()
+      };
+
+      const axis = {
+         x: g =>
+            g
+               .attr(
+                  "transform",
+                  `translate(0, ${dimension.height - margin.bottom})`
+               )
+               .call(d3.axisBottom(scale.x)),
+         y: g =>
+            g
+               .attr("transform", `translate(${margin.left}, 0)`)
+               .call(d3.axisLeft(scale.y))
+      };
+
+      svg.select(".x-axis").call(axis.x);
+      svg.select(".y-axis").call(axis.y);
+
+      svg.selectAll("rect")
+         .data(data)
+         .join("rect")
+         .attr("x", d => scale.x(d["Year"] || d["Cause"]))
+         .attr("width", scale.x.bandwidth())
+         .attr("y", d => scale.y(d["Total Claim Cost"] || d["Cost"]))
+         .attr("height", d =>
+            scale.y(dimension.height - d["Total Claim Cost"] || d["Cost"])
+         );
+
+      d3.selectAll("rect").on("click", d => HandleClick(d));
+   }, [data, dimension, HandleClick]);
+
+   return (
+      <BigChart>
+         <div ref={wrapperRef} style={{ height: "100%" }}>
+            <svg ref={svgRef}>
+               <g className="x-axis" />
+               <g className="y-axis" />
+            </svg>
+         </div>
+      </BigChart>
+   );
+};
+
+export default BarChart;
