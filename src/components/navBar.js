32e8cd94feda83ea6c9b38aa9fc17106ca300f91
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-15 16:35:05 +0100

    add button for clear selection

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,49 +1,60 @@
-import React from "react"
-import { NavLink, useRouteMatch } from "react-router-dom"
-import styled from "styled-components"
+import React, { useContext } from "react";
+import { NavLink, useRouteMatch } from "react-router-dom";
+import styled from "styled-components";
+import { QDocContext } from "../enigma/docProvider";
 
 const NavBar = () => {
-	let match = useRouteMatch()
+   let match = useRouteMatch();
+   const qdoc = useContext(QDocContext);
 
-	return (
-		<Div>
-			<Links exact to={`${match.url}`}>
-				Summary
-			</Links>
-			<Links exact to={`${match.url}/claims-costs-analysis`}>
-				Claims Cost Analysis
-			</Links>
-			<Links exact to={`${match.url}/fraud-analysis`}>
-				Fraud Analysis
-			</Links>
-		</Div>
-	)
-}
+   const handleClick = async () => {
+      try {
+         await qdoc.clearAll();
+         console.log("cleared selection");
+      } catch (e) {
+         console.error("Error on clear selection", e);
+      }
+   };
+   return (
+      <Div>
+         <Links exact to={`${match.url}`}>
+            Summary
+         </Links>
+         <Links exact to={`${match.url}/claims-costs-analysis`}>
+            Claims Cost Analysis
+         </Links>
+         <Links exact to={`${match.url}/fraud-analysis`}>
+            Fraud Analysis
+         </Links>
+         <button onClick={handleClick}>Clear Selection</button>
+      </Div>
+   );
+};
 
-export default NavBar
+export default NavBar;
 
 const Div = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	height: 7vh;
-	border-bottom: 1px solid #e2e8f0;
-	box-sizing: border-box;
-`
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   height: 7vh;
+   border-bottom: 1px solid #e2e8f0;
+   box-sizing: border-box;
+`;
 const Links = styled(NavLink)`
-	/* margin: 0.5rem; */
-	height: 100%;
-	text-decoration: none;
-	display: flex;
-	text-align: center;
-	align-items: center;
-	padding: 0 0.5rem;
-	margin: 0 0.25rem;
-	font-size: 1rem;
-	font-weight: bold;
-	color: #4a5568;
-	&.active {
-		color: #3182ce;
-		border-bottom: 1px solid #2b6cb0;
-	}
-`
+   /* margin: 0.5rem; */
+   height: 100%;
+   text-decoration: none;
+   display: flex;
+   text-align: center;
+   align-items: center;
+   padding: 0 0.5rem;
+   margin: 0 0.25rem;
+   font-size: 1rem;
+   font-weight: bold;
+   color: #4a5568;
+   &.active {
+      color: #3182ce;
+      border-bottom: 1px solid #2b6cb0;
+   }
+`;
