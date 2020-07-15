1eba915fd50369378689c56b983a6ff882818cb2
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-15 16:31:36 +0100

    minors

--- src/hooks/useHyperCubeDataObject.js
+++ src/hooks/useHyperCubeDataObject.js
@@ -1,21 +1,21 @@
-import { useState, useEffect, useContext } from "react"
-import { QDocContext } from "../enigma/docProvider"
+import { useState, useEffect, useContext } from "react";
+import { QDocContext } from "../enigma/docProvider";
 
 const useHyperCubeDataObject = (object, definition) => {
-	const [data, setData] = useState()
-	const qdoc = useContext(QDocContext)
+   const [data, setData] = useState();
+   const qdoc = useContext(QDocContext);
 
-	useEffect(() => {
-		const GetDataFromObject = async () => {
-			// const session = await qdoc.createSessionObject(SummaryClaimsOpened)
-			const layout = await qdoc.getObject(object)
-			const data = await layout.getHyperCubeData(definition)
-			setData(data[0].qMatrix)
-		}
-		GetDataFromObject()
-	}, [qdoc, object, definition])
+   useEffect(() => {
+      const GetDataFromObject = async () => {
+         const layout = await qdoc.getObject(object);
+         const data = await layout.getHyperCubeData(definition);
+         console.log(data);
+         setData(data[0].qMatrix);
+      };
+      GetDataFromObject();
+   }, [qdoc, object, definition]);
 
-	return data
-}
+   return data;
+};
 
-export default useHyperCubeDataObject
+export default useHyperCubeDataObject;
