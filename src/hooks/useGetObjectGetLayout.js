85ab656be02a8d98aed1bac4c039c72b6c77a736
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-15 11:09:00 +0100

    minor

--- src/hooks/useGetObjectGetLayout.js
+++ src/hooks/useGetObjectGetLayout.js
@@ -1,25 +1,24 @@
 import { useState, useEffect, useContext } from "react";
 import { QDocContext } from "../enigma/docProvider";
 
-const useGetObjectGetLayout = object => {
-   const [data, setData] = useState();
-   const qdoc = useContext(QDocContext);
+const useGetObjectGetLayout = (object) => {
+	const [data, setData] = useState();
+	const qdoc = useContext(QDocContext);
 
-   useEffect(() => {
-      (async function() {
-         const model = await qdoc.getObject(object);
-         const layout = await model.getLayout();
-         const qTitle = layout.qHyperCube.qMeasureInfo[0].qFallbackTitle;
-         console.log(layout.qHyperCube);
+	useEffect(() => {
+		(async function () {
+			const model = await qdoc.getObject(object);
+			const layout = await model.getLayout();
+			const qTitle = layout.qHyperCube.qMeasureInfo[0].qFallbackTitle;
 
-         // TODO: get qDimensionInfo/qGroupFallbackTitles
-         //  get qMeasureInfo/qGroupFallbackTitles
-         const qMatrix = layout.qHyperCube.qDataPages[0].qMatrix;
-         setData([qTitle, qMatrix]);
-      })();
-   }, [qdoc, object]);
+			// TODO: get qDimensionInfo/qGroupFallbackTitles
+			//  get qMeasureInfo/qGroupFallbackTitles
+			const qMatrix = layout.qHyperCube.qDataPages[0].qMatrix;
+			setData([qTitle, qMatrix]);
+		})();
+	}, [qdoc, object]);
 
-   return data;
+	return data;
 };
 
 export default useGetObjectGetLayout;
--- src/hooks/useLayoutSessionObject.js
+++ src/hooks/useLayoutSessionObject.js
@@ -1,20 +1,20 @@
-import { useState, useEffect, useContext } from "react"
-import { QDocContext } from "../enigma/docProvider"
+import { useState, useEffect, useContext } from "react";
+import { QDocContext } from "../enigma/docProvider";
 
 const useLayoutSessionObject = (definition) => {
-	const [data, setData] = useState()
-	const qdoc = useContext(QDocContext)
+	const [data, setData] = useState();
+	const qdoc = useContext(QDocContext);
 
 	useEffect(() => {
 		const DataFromSessionObject = async () => {
-			const app = await qdoc.createSessionObject(definition)
-			const layout = await app.getLayout()
-			const dataset = layout.qHyperCube.qDataPages[0].qMatrix
-			setData(dataset)
-		}
-		DataFromSessionObject()
-	}, [qdoc, definition])
-	return data
-}
+			const app = await qdoc.createSessionObject(definition);
+			const layout = await app.getLayout();
+			const dataset = layout.qHyperCube.qDataPages[0].qMatrix;
+			setData(dataset);
+		};
+		DataFromSessionObject();
+	}, [qdoc, definition]);
+	return data;
+};
 
-export default useLayoutSessionObject
+export default useLayoutSessionObject;
