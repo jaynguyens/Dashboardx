c578ad610dc3256f285794389a0c78428c51dca3
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-16 13:25:32 +0100

    minor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react"
-import Routes from "./Routes"
+import React from "react";
+import Routes from "./Routes";
 // App function render the routing and layout of the dashboard
 const App = () => {
-	return (
-		<>
-			<Routes />
-		</>
-	)
-}
+   return (
+      <>
+         <Routes />
+      </>
+   );
+};
 
-export default App
+export default App;
--- src/enigma/definition/summary/dataFromHyperCubeObject.js
+++ src/enigma/definition/summary/dataFromHyperCubeObject.js
@@ -1,13 +1,13 @@
 const HyperCubeData = {
-	qPath: "/qHyperCubeDef",
-	qPages: [
-		{
-			qTop: 0,
-			qLeft: 0,
-			qWidth: 10,
-			qHeight: 10,
-		},
-	],
-}
+   qPath: "/qHyperCubeDef",
+   qPages: [
+      {
+         qTop: 0,
+         qLeft: 0,
+         qWidth: 10,
+         qHeight: 10
+      }
+   ]
+};
 
-export default HyperCubeData
+export default HyperCubeData;
