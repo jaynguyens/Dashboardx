b34efc5ca0393e9d9a8a6dff26f63a61dd648b4a
Jay Nguyen <jaynguyens+app@pm.me>
2020-07-17 08:53:42 +0100

    wip

--- src/pages/fraudAnalysis/index.js
+++ src/pages/fraudAnalysis/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { QDocContext } from "../../enigma/docProvider";
 
 const FraudAnalysis = () => {
